4bb5320de78190e9a58fc746c5a5e278e5424547
Yohrman Daniel Garcia Vergara <daniel.garciav@vivaaerobus.com>
2023-05-22 11:18:24 -0600

    change to local flights

--- app/Flights.php
+++ app/Flights.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flights extends Model
 {
-    protected $connection = 'sqlsrv2';
+    protected $connection = 'sqlsrv';
 
     protected $table = 'tbl_AllFlights';
 }
